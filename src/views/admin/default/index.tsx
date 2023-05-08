88f4780210e9c0650591f8fcbb663863f9222c4b
14Richa <onlinericha19@gmail.com>
2023-05-08 14:01:01 +0530

    changed text in about page

--- src/views/admin/default/index.tsx
+++ src/views/admin/default/index.tsx
@@ -1,6 +1,4 @@
 
-
-// Chakra imports
 import { Box, Text, SimpleGrid  } from '@chakra-ui/react';
 import Card from 'components/card/Card';
 
@@ -13,21 +11,22 @@ export default function UserReports() {
 
 		<Box pt={{ base: '80px', xl: '50px' }} mt='80px'>
 		<SimpleGrid columns={3} spacing={4}>
+
 				<div style={{ maxWidth: '800px', margin: '0 auto', marginTop: '60px', backgroundColor: 'transparent' }} >
 		 			<img src ={imageLeft}  alt = "image1" style={{ height: 400}}/>
 		 		</div>
+
 		  <Card maxWidth='800px' mx='auto' mt= '60px'>
 		    <Text color="000000" fontWeight='bold' fontSize='2xl' mt='10px' mb='4px' textAlign='center'>
 		      Welcome
 		    </Text>
-		    <Text color="#8E8E8E" fontSize='md' me='26px' lineHeight='1.7' mb='40px'>				
-			This application allows you to visualize the relationships between genes and phenotypes in a user-friendly way.
-				Using this application, you can select a gene and one or more phenotypes to display on the heatmap.
-			<br/>
-			The heatmap will show the intensity of the relationship between the selected gene and phenotypes, with darker colors indicating stronger relationships.
-			<br/>
-			The application is especially useful for researchers and scientists who study genetics and want to better understand the relationships between genes and phenotypes.
-			With my heatmap visualization, you can quickly identify patterns and trends in the data that might not be immediately apparent from raw data alone.
+		    <Text color="#8E8E8E" fontSize='md' me='26px' lineHeight='1.5' mb='40px' textAlign='center'>	
+			This application offers an intuitive and user-friendly way to visualize the complex relationships between genes and phenotypes. By selecting a specific gene and one or more phenotypes, users can easily generate a heatmap that displays the strength of the relationships between the selected gene and phenotypes. 
+			The heatmap uses a color gradient to represent different intensities, with darker colors indicating stronger relationships.
+			In addition to the heatmap, the summary statistics page provides users with insightful charts that offer a deeper understanding of the dataset from different perspectives. 
+			These charts can be used to identify trends and outliers, and to gain a better understanding of the distribution of phenotype data.
+			The application is particularly valuable to researchers and scientists in the field of genetics who require a comprehensive tool to analyze and interpret complex data. 
+			Heatmap visualization enables users to quickly identify patterns and trends that may not be immediately evident from raw data alone, and provides a valuable tool for gaining new insights into the complex relationships between genes and phenotypes.
 		    </Text>
 		  </Card>
 		  <div style={{ maxWidth: '800px', margin: '0 auto', marginTop: '60px', backgroundColor: 'transparent' }} >				
